fbe8f75eeb98512af8f6ff42c1fd71ff225b01d7
Natalia Galan <n8galan@gmail.com>
2020-07-03 21:09:37 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,7 +15,8 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  %w[DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  people = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  %wpeople
 end
 
 def aoh_lookup(aoh, row, key)
